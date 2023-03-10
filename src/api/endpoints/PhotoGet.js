0ba65ddc48703168b50fc351792b5d456d4bb668
Kevin Alexsander De Jesus1 <kjesus1@Lenovo.com>
2023-03-10 15:07:47 -0300

    photo get

--- src/api/endpoints/PhotoGet.js
+++ src/api/endpoints/PhotoGet.js
@@ -1,1 +1,25 @@
 import React from 'react'
+
+const PhotoGet = () => {
+  const [ id, setId ] = React.useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    fetch(`https://dogsapi.origamid.dev/json/api/photo/${id}`)
+      .then( res => {
+        console.log( res )
+        return res.json()
+      })
+      .then( json => console.log( json ))
+  }
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <input type='text' value={ id } onChange={({ target }) => setId( target.value )}/>
+      <button>Enviar</button>
+    </form>
+  )
+}
+
+export default PhotoGet
