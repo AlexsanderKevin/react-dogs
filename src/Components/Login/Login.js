4234d24c34312df8e6226b560ca911e49a670950
Kevin Alexsander De Jesus1 <kevin13348@gmail.com>
2023-03-15 13:24:16 -0300

    styled the form components

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -11,7 +11,7 @@ const Login = () => {
       <Routes>
         <Route path='/' element={ <LoginForm/> } />
         <Route path='criar' element={ <LoginCreate/> } />
-        <Route path='perdeu' element={ <LoginPasswordLost /> } />
+        <Route path='perdeu' element={ <LoginPasswordLost/> } />
         <Route path='resetar' element={ <LoginPasswordReset/> } />
       </Routes>
     </div>
