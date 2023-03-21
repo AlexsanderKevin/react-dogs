3238919c1a731b498614aeb17e35fdaec440d424
Kevin Alexsander De Jesus1 <kevin13348@gmail.com>
2023-03-21 09:45:00 -0300

    styled the login form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -5,6 +5,8 @@ import Button from '../Forms/Button'
 import Input from '../Forms/Input'
 import { UserContext } from '../../UserContext'
 import Error from '../Helper/Error'
+import styles from './LoginForm.module.css'
+import stylesBtn from '../Forms/Button.module.css'
 
 const LoginForm = () => {
   const username = useForm()
@@ -23,7 +25,7 @@ const LoginForm = () => {
   return (
     <section className='animeLeft'>
       <h1 className='title'> Login </h1>
-      <form action='' onSubmit={ handleSubmit }>
+      <form className={ styles.form } onSubmit={ handleSubmit }>
         <Input
           label='Usuário'
           type='text'
@@ -44,7 +46,13 @@ const LoginForm = () => {
         <Error error={ error } />
       </form>
 
-      <Link to='/login/criar'>Cadastro</Link>
+      <Link className={ styles.perdeu } to='/login/perdeu'>Perdeu a Senha?</Link>
+
+      <div className={ styles.cadastro }>
+        <h2 className={ styles.subTitle }>Cadastre-se</h2>
+        <p>Ainda não possui uma conta? Cadastre-se no site.</p>
+        <Link className={ stylesBtn.button } to='/login/criar'>Cadastro</Link>
+      </div>
     </section>
   )
 }
