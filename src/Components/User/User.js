cab60827be5237d95eb748596534ae575c04544f
Kevin Alexsander De Jesus1 <kevin13348@gmail.com>
2023-03-24 11:05:48 -0300

    protected route

---
+++ src/Components/User/User.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Feed from '../Feed/Feed'
+import UserHeader from './UserHeader'
+
+const User = () => {
+  return (
+    <section className='container'>
+      <UserHeader />
+      <Routes>
+        <Route path='/' element={ <Feed/> } />
+      </Routes>
+    </section>
+  )
+}
+
+export default User
--- src/UserContext.js
+++ src/UserContext.js
@@ -68,6 +68,9 @@ export const UserStorage = ({ children }) => {
 
         finally { setLoading( false ) }
       }
+      else {
+        setLogin( false )
+      }
     }
     autoLogin()
   }, [ userLogout ])
