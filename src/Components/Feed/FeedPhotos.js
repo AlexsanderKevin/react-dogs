93e53d0ed56e1242493dd95d177222079981fd09
Kevin Alexsander De Jesus1 <kevin13348@gmail.com>
2023-03-30 09:48:40 -0300

    0821 FeedModal 1

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,7 +6,7 @@ import Error from '../Helper/Error'
 import Loading from '../Helper/Loading'
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch()
 
   React.useEffect(() => {
@@ -30,6 +30,7 @@ const FeedPhotos = () => {
         <FeedPhotosItem
           key={ photo.id }
           photo={ photo }
+          setModalPhoto={ setModalPhoto }
         />
       ) )}
     </ul>
