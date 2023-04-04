2d0cf31a3a225374ddfa88f3bd43578693da4d2f
Kevin Alexsander De Jesus1 <kevin13348@gmail.com>
2023-04-04 09:05:00 -0300

    0824 foto delete

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -2,8 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import PhotoComments from './PhotoComments'
 import styles from './PhotoContent.module.css'
+import { UserContext } from '../../UserContext'
+import PhotoDelete from './PhotoDelete'
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext( UserContext )
   const { photo, comments } = data
 
   return (
@@ -11,15 +14,22 @@ const PhotoContent = ({ data }) => {
       <div className={ styles.img }>
         <img src={ photo.src } alt='' />
       </div>
+
       <div className={ styles.details }>
         <div>
           <p className={ styles.author }>
-            <Link to={ `/perfil/${photo.author}` }>@{ photo.author }</Link>
+            { user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={ photo.id } />
+            ) : (
+              <Link to={ `/perfil/${photo.author}` }>@{ photo.author }</Link>
+            )}
             <span className={ styles.visualizations }>{ photo.acessos }</span>
           </p>
+
           <h1 className='title'>
             <Link to={ `/foto/${photo.id}` }>{ photo.title }</Link>
           </h1>
+
           <ul className={ styles.attributes }>
             <li>{ photo.peso } kg</li>
             <li>{ photo.idade } anos</li>
